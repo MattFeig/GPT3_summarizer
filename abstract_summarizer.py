aeda0db46027aabb72cab12a502e8e3db15161b1
MattFeig <30275344+MattFeig@users.noreply.github.com>
2023-02-01 14:44:23 -0800

    better handle arguments

--- abstract_summarizer.py
+++ abstract_summarizer.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 
-import sys, os
+import sys, os, argparse
 from PIL import Image
 from pytesseract import pytesseract
 import openai
 
-def abstract_summarizer(arguments):
+def abstract_summarizer():
+
+
+    arg_parser = argparse.ArgumentParser(description="""this script creates a 2 sentence GPT3 summary from an
+                image of a scientific abstract --- Example call: python3 abstract_summarizre.py path/to/png""")
+    if len(sys.argv[1:])==0:
+        print('\nArguments required. Use -h option to print FULL usage.\n')
+    arg_parser.add_argument('image_path', type=os.path.abspath, help = 'png file path of a scientific abstract')
+    args = arg_parser.parse_args()
+
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    image_path = arguments[0]
-    img = Image.open(image_path)
+    
+    img = Image.open(args.image_path)
     img_text = pytesseract.image_to_string(img)
     img_text = img_text.replace("\n", "")
     prompt = f"Summarize the following science journal abstract in 2 sentences: {img_text}"
@@ -17,4 +26,4 @@ def abstract_summarizer(arguments):
     print(text)
 
 if __name__ == '__main__':
-    sys.exit(abstract_summarizer(sys.argv[1:]))
+    sys.exit(abstract_summarizer())
